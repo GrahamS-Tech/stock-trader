81cd94ac5cc2d4c1c080160ff8ff4f5b5f03caf3
kendric84 <graham.k.smith@live.com>
2023-04-23 11:41:54 -0400

    Weekly update

--- ClientApp/src/components/MyPortfolio.js
+++ ClientApp/src/components/MyPortfolio.js
@@ -1,11 +1,66 @@
 import React from 'react';
-
+import { ButtonToolbar, ButtonGroup, Button, Table } from 'react-bootstrap';
+import monitor from "./Assets/Images/stock-chart.jpg";
 
 //View my current holdings
 //Search for new stocks
 
 export default function MyPortfolio() {
     return (
-        <h1>My Portfolio</h1>
+        < div className="container-fluid w-50 justify-content-center">
+            <h3 className="m-2">$10,857.39</h3>
+            <h5 className="m-2">+10% Today</h5>
+            <div className="d-block justify-content-center">
+                <img className="img-fluid" src={monitor} alt="chart placeholder"></img>
+                <ButtonToolbar className="my-2">
+                    <ButtonGroup className="mx-auto" aria-label="Date range group">
+                        <Button variant="secondary">d</Button>
+                        <Button variant="secondary">w</Button>
+                        <Button variant="secondary">m</Button>
+                        <Button variant="secondary">ytd</Button>
+                        <Button variant="secondary">y</Button>
+                        <Button variant="secondary">all</Button>
+                    </ButtonGroup>
+                </ButtonToolbar>
+            </div>
+            <hr></hr>
+            <div>
+                <h3>My portfolio</h3>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Shares</th>
+                            <th>Value</th>
+                            <th>Trade</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="text-center" colSpan={4}>No items in your portfolio</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+            <hr></hr>
+            <div>
+                <h3>My watch list</h3>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Shares</th>
+                            <th>Value</th>
+                            <th>Trade</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="text-center" colSpan={4}>No items in your watch list</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        </div>
     );
 }
