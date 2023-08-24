35b556f0c145b0a48678861f6f3ab5e10d64d6c7
Graham Smith <graham.k.smith@live.com>
2023-08-24 08:51:29 -0500

    Updates to transaction controller & completion of trade shares functionality

--- ClientApp/src/Components/TradeSharesModal.js
+++ ClientApp/src/Components/TradeSharesModal.js
@@ -2,40 +2,25 @@
 import { Modal, Alert, ButtonGroup, Button, Form } from "react-bootstrap";
 import { getLastClose } from "../Adapters/StockData";
 import { formatCurrency } from "../Adapters/StringToCurrency"
+import { useAuth } from "./AuthContext";
+import { addTransaction } from "../Adapters/Transaction"
 
 export default function TradeSharesModal(props) {
+    const { currentUser } = useAuth();
     const [error, setError] = useState("");
     const [success, setSuccess] = useState("");
+    //combine state objects into one state object
     const [buyOrSell, setBuyOrSell] = useState("");
-    const [marketPrice, setMarketPrice] = useState();
-    const [formattedMarketPrice, setFormattedMarketPrice] = useState();
-    const [estimatedCost, setEstimatedCost] = useState();
+    const [marketPrice, setMarketPrice] = useState(0);
+    const [formattedMarketPrice, setFormattedMarketPrice] = useState("");
     const sharesRef = useRef(0);
-
-    function handleBuySellChange(e) {
-        if (error === "Buy or sell?") setError(""); 
-        setBuyOrSell(e.target.value)
-    }
-
-    function handleTrade(e) {
-        e.preventDefault();
-        if (!buyOrSell) {
-            setError("Buy or sell?")
-        }
-    }
-
-    function handleSharesChange() {
-        if (sharesRef.current.value) {
-            setEstimatedCost(formatCurrency(marketPrice * sharesRef.current.value))
-        }
-
-    }
+    const [estimatedCost, setEstimatedCost] = useState(null);
 
     const getPrice = useCallback(async() => {
         try {
             const lastClose = await getLastClose(props.selectedTicker)
-            await setMarketPrice(lastClose);
-            await setFormattedMarketPrice(formatCurrency(lastClose));
+            setMarketPrice(lastClose);
+            setFormattedMarketPrice(formatCurrency(lastClose));
         } catch (err) {
             setError("Can't get price. Try again later")
             console.error(err)
@@ -48,10 +33,65 @@ export default function TradeSharesModal(props) {
         }
     },[props.isModalOpen])
 
+    function handleBuySellChange(e) {
+        if (error === "Buy or sell?") setError(""); 
+        setBuyOrSell(e.target.value)
+    }
+
+    function handleClose() {
+        setError("");
+        setSuccess("");
+        setBuyOrSell("");
+        setMarketPrice(0)
+        setFormattedMarketPrice("")
+        setEstimatedCost("")
+        props.closeTradeModal();
+    }
+
+    async function handleTrade(e) {
+        e.preventDefault();
+        if (!buyOrSell) {
+            setError("Buy or sell?")
+            return
+        }
+
+        let shares = buyOrSell === "Buy" ? sharesRef.current.value : (sharesRef.current.value * -1)
+        const transactionType = buyOrSell === "Buy" ? "bought" : "sold"
+
+        const details = JSON.stringify({
+            "Ticker": props.selectedTicker,
+            "Shares": shares,
+            "TransactionType": buyOrSell,
+            "MarketValue": marketPrice,
+            "ShareName": props.selectedName
+        })
+
+        try {
+            const response = await addTransaction(currentUser, details)
+            if (response.Status === "success") {
+                setSuccess(`${sharesRef.current.value} share(s) of ${props.selectedTicker} has successfully been ${transactionType}`)
+            } else {
+                console.error(response.Message)
+                setError("Unable to complete transaction")
+            }
+            return
+        } catch (err) {
+            console.error(err)
+            setError("Unable to complete transaction")
+        }
+    }
+
+    function handleSharesChange() {
+        if (sharesRef.current.value) {
+            setEstimatedCost(formatCurrency(marketPrice * sharesRef.current.value))
+        }
+    }
+
+
 
     return (
         <>
-            <Modal show={props.isModalOpen} onHide={() => { setError("");  setBuyOrSell(""); props.closeTradeModal(); }}>
+            <Modal show={props.isModalOpen} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <div className="container-fluid justify-content-center">
                         <Modal.Title>
@@ -64,12 +104,13 @@ export default function TradeSharesModal(props) {
                 <Modal.Body>
                     <Form onSubmit={handleTrade}>
                             <ButtonGroup onClick={handleBuySellChange}>
-                                <Button name="group1" variant={buyOrSell == "Buy" ? "success" : "secondary" } value="Buy">Buy</Button>
-                                <Button name="group1" variant={buyOrSell == "Sell" ? "success" : "secondary"} value="Sell">Sell</Button>
+                                <Button name="group1" variant={buyOrSell === "Buy" ? "success" : "secondary" } value="Buy">Buy</Button>
+                                <Button name="group1" variant={buyOrSell === "Sell" ? "success" : "secondary"} value="Sell">Sell</Button>
                             </ButtonGroup>
                         <Form.Group>
                             <Form.Label>Shares to trade</Form.Label>
                             <Form.Control disabled type="text" value={props.selectedTicker}></Form.Control>
+                            <Form.Control disabled type="text" value={props.selectedName}></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Number of shares</Form.Label>
