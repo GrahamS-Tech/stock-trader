c65bbceb155fb54fef4846e03edae6ccb9a4ce25
Graham Smith <graham.k.smith@live.com>
2023-08-25 12:48:30 -0500

    Fixed NHibernate configuration for new fiat DB tables. Finalized recording new Fiat transactions.

---
+++ ClientApp/src/Adapters/FiatTransaction.js
@@ -0,0 +1,16 @@
+﻿export async function addFiatTransaction(currentUser, details) {
+    try {
+        const response = await fetch("https://localhost:7247/api/FiatTransaction/AddFiatTransaction", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + currentUser
+            },
+            body: details
+        });
+        const result = await response.json();
+        return result
+    } catch (err) {
+        console.error(err)
+    }
+}
